5daab1b2ac4e1420bb76bf9f0e0cd857df1d74d7
Oscar Bergström <oscar@obstrom.com>
2022-05-15 16:43:17 +0200

    React 18 render dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './components/App';
+import App from "./components/App";
 
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
